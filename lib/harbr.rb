a0ab22272bbf433bef314e1ded8d183098aeca11
Delaney Burke <delaney@vidtreone.com>
2023-12-03 20:41:52 +0200

    show the ports

--- lib/harbr.rb
+++ lib/harbr.rb
@@ -9,5 +9,50 @@ module Harbr
     attr_accessor :name, :host_header, :ip, :port  
    end
 
+   class Port
+    
+    include Dddr::Entity
+    attr_accessor :host_header, :number
+
+    queries do
+      def has_port_number?(number)
+        all.find {|port| port.number == number.to_i}
+      end
+    end
+    class Pool
+      def initialize(port_range=50000..51000)
+        
+        @ports = Port::Repository.new        
+        
+        port_range.each do |number|
+          port = Port.new        
+          port.number = number
+          unless @ports.has_port_number? number
+            @ports.add(port) 
+            puts port.number.to_s + " added!"
+          end
+
+        end
+
+      end
+    
+      def get_port(host_header)
+        
+      end
+    
+      def return_port(port)
+        
+      end
+    
+      def ports
+        @ports.all
+      end
+
+    end
+
+
+  end
+  
+   
    # Your code goes here...
 end
--- spec/harbr_spec.rb
+++ spec/harbr_spec.rb
@@ -1,11 +1,17 @@
 # frozen_string_literal: true
 
 RSpec.describe Harbr do
-  it "has a version number" do
-    expect(Harbr::VERSION).not_to be nil
-  end
+  
+end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+
+RSpec.describe Harbr::Port::Pool do
+
+  it 'should have 100 ports' do
+    
+    pool = Harbr::Port::Pool.new
+
+    expect(pool.ports.count).to eq(1001)
+
   end
 end
