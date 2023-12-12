f2198e5bbd357b8353ff7c5cc35d5e0afd54636a
Delaney Burke <delaney@vidtreone.com>
2023-12-12 15:26:46 +0200

    add next and current

--- spec/harbr_spec.rb
+++ spec/harbr_spec.rb
@@ -3,11 +3,9 @@
 RSpec.describe Harbr do
 end
 
-
-
 module Runit
   class Run
-    def initialize(container,port)
+    def initialize(container, port)
       @container_name = container
       @port = port
     end
@@ -19,22 +17,20 @@ module Runit
         cd /var/harbr/#{@container_name}/current
         exec ./exe/run #{@port}
       SCRIPT
-      
     end
 
     def link
       "ln -s /etc/sv/harbr/#{@container_name} /etc/service/#{@container_name}"
     end
-
   end
 
   module Next
     class Run
-      def initialize(container,port)
+      def initialize(container, port)
         @container_name = container
         @port = port
       end
-  
+
       def to_s
         script_template = <<~SCRIPT
           #!/bin/sh
@@ -42,39 +38,55 @@ module Runit
           cd /var/harbr/#{@container_name}/next
           exec ./exe/run #{@port}
         SCRIPT
-        
       end
-  
+
       def link
         "ln -s /etc/sv/harbr/#{@container_name}/next /etc/service/next.#{@container_name}"
       end
+    end
   end
-end
 
+  RSpec.describe Runit::Run do
+    it "should create a run script" do
+      container_name = "test"
+      port = 3000
 
-RSpec.describe Runit::Run do
-  it "should create a run script" do
-    container_name = "test"
-    port = 3000
-
-    run = Runit::Run.new(container_name, port)
-    expect(run.to_s).to eq <<~SCRIPT
+      run = Runit::Run.new(container_name, port)
+      expect(run.to_s).to eq <<~SCRIPT
         #!/bin/sh
         exec 2>&1
         cd /var/harbr/#{container_name}/current
         exec ./exe/run #{port}
       SCRIPT
-  end
-  
-   it "should link the service" do
-    container_name = "test"
-    port = 3000
-    run = Runit::Run.new(container_name,port)
-    expect(run.link).to eq "ln -s /etc/sv/harbr/#{container_name} /etc/service/#{container_name}"
-
     end
-    
+
+    it "should link the service" do
+      container_name = "test"
+      port = 3000
+      run = Runit::Run.new(container_name, port)
+      expect(run.link).to eq "ln -s /etc/sv/harbr/#{container_name} /etc/service/#{container_name}"
+    end
+
+    context "Next" do
+      it "should create a run script" do
+        container_name = "test"
+        port = 3000
+
+        run = Runit::Next::Run.new(container_name, port)
+        expect(run.to_s).to eq <<~SCRIPT
+          #!/bin/sh
+          exec 2>&1
+          cd /var/harbr/#{container_name}/next
+          exec ./exe/run #{port}
+        SCRIPT
+      end
+
+      it "should link the service" do
+        container_name = "test"
+        port = 3000
+        run = Runit::Next::Run.new(container_name, port)
+        expect(run.link).to eq "ln -s /etc/sv/harbr/#{container_name}/next /etc/service/next.#{container_name}"
+      end
+    end
+  end
 end
-
-
-
