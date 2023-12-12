af65132ef64c38d058af9338e4cc8313c412717e
Delaney Burke <delaney@vidtreone.com>
2023-12-12 17:49:46 +0200

    add runnit scripts

--- lib/harbr.rb
+++ lib/harbr.rb
@@ -17,6 +17,90 @@ module Harbr
   DEFAULT_DIRECTORY = "/var/harbr"
   DEFAULT_DIRECTORY_DATA_DIR = "#{DEFAULT_DIRECTORY}/.data"
   class Error < StandardError; end
+
+  def self.highest_numbered_directory(path)
+    directories = Dir.glob("#{path}/*").select { |entry| File.directory?(entry) }
+    directories.max_by { |entry| entry[/\d+/].to_i }
+  end
+
+  module Runit
+    class Run
+      def initialize(container, port)
+        @container_name = container
+        @port = port
+      end
+
+      def to_s
+        script_template = <<~SCRIPT
+          #!/bin/sh
+          exec 2>&1
+          cd /var/harbr/#{@container_name}/current
+          exec ./exe/run #{@port}
+        SCRIPT
+      end
+
+      def link
+        "ln -s /etc/sv/harbr/#{@container_name} /etc/service/#{@container_name}"
+      end
+    end
+
+    class Log
+      def initialize(container, port)
+        @container_name = container
+      end
+
+      def to_s
+        script_template = <<~SCRIPT
+          #!/bin/sh
+          exec svlogd -tt /var/log/harbr/#{@container_name}/next/
+        SCRIPT
+      end
+
+      def link
+        "ln -s /etc/sv/harbr/#{@container_name}/log /etc/service/#{@container_name}/log"
+      end
+    end
+
+    module Next
+      class Run
+        def initialize(container, port)
+          @container_name = container
+          @port = port
+        end
+
+        def to_s
+          script_template = <<~SCRIPT
+            #!/bin/sh
+            exec 2>&1
+            cd /var/harbr/#{@container_name}/next
+            exec ./exe/run #{@port}
+          SCRIPT
+        end
+
+        def link
+          "ln -s /etc/sv/harbr/#{@container_name}/next /etc/service/next.#{@container_name}"
+        end
+      end
+      class Log
+        def initialize(container, port)
+          @container_name = container
+        end
+  
+        def to_s
+          script_template = <<~SCRIPT
+            #!/bin/sh
+            exec svlogd -tt /var/log/harbr/#{@container_name}/next/
+          SCRIPT
+        end
+  
+        def link
+
+          "ln -s /etc/sv/harbr/#{container_name}/next/log/ /etc/service/next.#{container_name}/log"
+        end
+      end
+  
+    end
+  end
 end
 
 Dddr.configure do |config|
