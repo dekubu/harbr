7627caf4782d81d99e864ec35f721ed760b72686
Delaney Burke <delaney@vidtreone.com>
2023-12-14 02:01:00 +0200

    release 0.0.78

--- lib/harbr.rb
+++ lib/harbr.rb
@@ -14,7 +14,7 @@ require_relative "harbr/next/job"
 
 # Harbr module for managing containers, jobs, ports, and 2s
 module Harbr
-  DEFAULT_DIRECTORY = "/var/harbr"
+  DEFAULT_DIRECTORY = "/var/harbr/containers"
   DEFAULT_DIRECTORY_DATA_DIR = "#{DEFAULT_DIRECTORY}/.data"
   class Error < StandardError; end
 
@@ -22,84 +22,7 @@ module Harbr
     directories = Dir.glob("#{path}/*").select { |entry| File.directory?(entry) }
     directories.max_by { |entry| entry[/\d+/].to_i }
   end
-
-  module Runit
-    class Run
-      def initialize(container, port)
-        @container_name = container
-        @port = port
-      end
-
-      def to_s
-        <<~SCRIPT
-          #!/bin/sh
-          exec 2>&1
-          cd /var/harbr/#{@container_name}/current
-          exec ./exe/run #{@port}
-        SCRIPT
-      end
-
-      def link
-        "ln -s /etc/sv/harbr/#{@container_name} /etc/service/#{@container_name}"
-      end
-    end
-
-    class Log
-      def initialize(container, port)
-        @container_name = container
-      end
-
-      def to_s
-        <<~SCRIPT
-          #!/bin/sh
-          exec svlogd -tt /var/log/harbr/#{@container_name}/next/
-        SCRIPT
-      end
-
-      def link
-        "ln -s /etc/sv/harbr/#{@container_name}/log /etc/service/#{@container_name}/log"
-      end
-    end
-
-    module Next
-      class Run
-        def initialize(container, port)
-          @container_name = container
-          @port = port
-        end
-
-        def to_s
-          <<~SCRIPT
-            #!/bin/sh
-            exec 2>&1
-            cd /var/harbr/#{@container_name}/next
-            exec ./exe/run #{@port}
-          SCRIPT
-        end
-
-        def link
-          "ln -s /etc/sv/harbr/#{@container_name}/next /etc/service/next.#{@container_name}"
-        end
-      end
-
-      class Log
-        def initialize(container, port)
-          @container_name = container
-        end
-
-        def to_s
-          <<~SCRIPT
-            #!/bin/sh
-            exec svlogd -tt /var/log/harbr/#{@container_name}/next/
-          SCRIPT
-        end
-
-        def link
-          "ln -s /etc/sv/harbr/#{container_name}/next/log/ /etc/service/next.#{container_name}/log"
-        end
-      end
-    end
-  end
+  
 end
 
 Dddr.configure do |config|
